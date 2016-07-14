fc4e692db4bcb55d1d2789297f0b29ec22a9d81a
Paul Mathews <pfcmathews@gmail.com>
2016-07-15 11:51:11 +1200

    addition looking good, no formal tests though because I don't have time to get tensorflow on travis

--- rnndatasets/addition/addition.py
+++ rnndatasets/addition/addition.py
@@ -38,8 +38,81 @@ def _gen_numpy_data(sequence_length, num_examples, seed=1001):
     mask[eg_idx, first_marks] = 1.0
     mask[eg_idx, second_marks] = 1.0
 
-    targets = np.einsum('ij, kj->k', random_data, mask)
+    # row wise dot them to get the targets
+    targets = np.einsum('ij, ij->i', random_data, mask)
 
     return np.stack((random_data, mask), axis=-1), targets
-    
-    
+
+
+def get_data_batches(sequence_length, batch_size, train_size, test_size,
+                     num_epochs=None):
+    """Gets tensors which batch up the data.
+    Probably will have to unpack them to feed into the rnn.
+    The test data is shuffled at each epoch.
+
+    Args:
+        sequence_length: the length of the sequences you want.
+        batch_size: precisely how to batch it up.
+        train_size: size of the training set
+        test_size: size of the test set.
+
+    Returns:
+        train, test: each is a tuple of data, targets. Data is
+        [sequence_length, batch_size, 2], targets are [batch_size].
+    """
+    all_data, all_targets = _gen_numpy_data(sequence_length,
+                                           train_size+test_size)
+    # now we have the data
+    # put it into variables
+    train_data = tf.Variable(all_data[:train_size, ...], trainable=False)
+    train_targets = tf.Variable(all_targets[:train_size], trainable=False)
+    test_data = tf.Variable(all_data[train_size:, ...], trainable=False)
+    test_targets = tf.Variable(all_targets[train_size:], trainable=False)
+
+    td_slice, tt_slice = tf.train.slice_input_producer(
+        [train_data, train_targets], num_epochs=num_epochs)
+
+    vd_slice, vt_slice = tf.train.slice_input_producer(
+        [test_data, test_targets], num_epochs=None)
+
+    td_batch, tt_batch = tf.train.batch([td_slice, tt_slice],
+                                        batch_size=batch_size)
+    # get the data into the right order
+    td_batch = tf.transpose(td_batch, [1, 0, 2])
+
+    vd_batch, vt_batch = tf.train.batch([vd_slice, vt_slice],
+                                        batch_size=batch_size)
+    vd_batch = tf.transpose(vd_batch, [1, 0, 2])
+
+    return (td_batch, tt_batch), (vd_batch, vt_batch)
+
+
+if __name__ == '__main__':
+    """not putting real tests in because can't be bothered getting tensorflow
+    on travis"""
+    train, test = get_data_batches(100, 50, 1000, 100, 1)
+    # we would then expect to get 20 batches of data before an exception.
+    sess = tf.Session()
+    sess.run(tf.initialize_all_variables())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    data, targets = sess.run(test)
+    assert data.shape == (100, 50, 2)
+    assert targets.shape == (50,)
+
+    try:
+        step = 0
+        while not coord.should_stop():
+            data, targets = sess.run(train)
+            assert data.shape == (100, 50, 2)
+            assert targets.shape == (50,)
+            step += 1
+    except tf.errors.OutOfRangeError:
+        assert step == 20
+    finally:
+        coord.request_stop()
+
+    coord.join(threads)
+    sess.close()
+    print('done')
