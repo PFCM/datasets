782425c64bded044a8b49af0f2bfe4c5f1117e17
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 08:59:31 +0000

    Adding tarfile member sanitization to extractall()

--- rnndatasets/ptb/ptb.py
+++ rnndatasets/ptb/ptb.py
@@ -54,7 +54,26 @@ def check_files():
                     gz_filename,
                     'http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz'),
                 'r:gz') as f:
-            f.extractall(path=_datapath(''))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, path=_datapath(""))
     return (os.path.join(data_dir, 'ptb.train.txt'),
             os.path.join(data_dir, 'ptb.valid.txt'),
             os.path.join(data_dir, 'ptb.test.txt'))
